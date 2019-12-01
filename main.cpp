4e6dc14318d5eff3fd7445d4021f1453df1320a6
Rodrigo Chichorro <reobcnegf@gmail.com>
2019-12-01 18:43:38 -0300

    juntando

--- main.cpp
+++ main.cpp
@@ -49,7 +49,7 @@ double paredeTras = -paredeFrente;
 
 //cor de cada face do cubo  , 6=marrom, 0=preto,4=azul, 7=amarelo, 3=laranja
 GLfloat colors[8][4] = { {0.0,0.0,0.0,1.0} , {1.0,0.0,0.0,1.0}, {1.0,1.0,0.0,1.0}, {1,0.5,0.0,1.0},
-{0.0,0.0,1.0,1.0} , {0.215,0.165,0.0,1.0},{0.198,0.089,0.007,1.0},{128,128,0,1.0}};
+{0.0,0.0,1.0,1.0} , {0.215,0.165,0.0,1.0},{0.198,0.145,0.087,1.0},{128,128,0,1.0}};
 
 //Vars de "animacao"
 double animaNavio,animaCardume,animaJetski,animaTorpedo = 0;
@@ -65,8 +65,18 @@ GLdouble viewer[] = {-1*radius, 0.0, 0.0}; //Posicao da camera
 GLdouble viewVector[] = {1.0, 0.0, 0.0}; //Vetor unitario que aponta de viewer para center
 //Iluminação
 bool Luz,ModoLuz = false; bool Luz1,Luz2 = false;
-
-bool Parser(const char * nome, int cor) {  
+//Textura
+GLuint texture[8];
+struct Image {
+    unsigned long sizeX;
+    unsigned long sizeY;
+    char *data;
+};
+typedef struct Image Image;
+#define checkImageWidth 64
+#define checkImageHeight 64
+GLubyte checkImage[checkImageWidth][checkImageHeight][3];
+bool Parser(const char * nome, int cor, int numTexture) {  
     //fstream f;
     //f.open (nome);
     FILE * f = fopen(nome, "r");
@@ -108,68 +118,140 @@ bool Parser(const char * nome, int cor) {
                 return false;
             }
             // o q faz com uvIndex[0]uvIndex[1]uvIndex[2] e normalIndex[0]normalIndex[1]normalIndex[2] ???
-            if (cor != 0)
-            glColor4fv(colors[cor]);
-            else
-                glColor4f(0.1,0.1,0.1,1);
-            glColorMaterial(GL_FRONT_AND_BACK,GL_AMBIENT_AND_DIFFUSE);
-            glBegin(GL_TRIANGLES);
-                glNormal3f (NormalVertices[normalIndex[0]-1][0],NormalVertices[normalIndex[0]-1][1],NormalVertices[normalIndex[0]-1][2]);
-                glVertex3f (Vertices[vertexIndex[0]-1][0], Vertices[vertexIndex[0]-1][1], Vertices[vertexIndex[0]-1][2]);
-                glNormal3f (NormalVertices[normalIndex[1]-1][0],NormalVertices[normalIndex[1]-1][1],NormalVertices[normalIndex[1]-1][2]);
-                glVertex3f (Vertices[vertexIndex[1]-1][0], Vertices[vertexIndex[1]-1][1], Vertices[vertexIndex[1]-1][2]);
-                glNormal3f (NormalVertices[normalIndex[2]-1][0],NormalVertices[normalIndex[2]-1][1],NormalVertices[normalIndex[2]-1][2]);
-                glVertex3f (Vertices[vertexIndex[2]-1][0], Vertices[vertexIndex[2]-1][1], Vertices[vertexIndex[2]-1][2]);
-            glEnd();
+            //  if (cor != 0)
+            //  glColor4fv(colors[cor]);
+            //  else
+            //      glColor4f(0.1,0.1,0.1,1);
+            glClearColor(1.0, 1.0, 1.0, 1.0);
+            glEnable(GL_TEXTURE_2D); //habilitar textura
+            glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+            glColor3ub(255, 255, 255);
+            if (numTexture == 4) glColor3f(1,0.3,0.0);
+            if (numTexture == 1) glColor3f(0.897,0.945,0.587);
+            if(numTexture != 2) {
+                glColorMaterial(GL_FRONT_AND_BACK,GL_AMBIENT_AND_DIFFUSE);
+                glBindTexture ( GL_TEXTURE_2D, texture[numTexture]);
+                //cerr<<TextureVertices[uvIndex[0]-1][0]<<" "<<TextureVertices[uvIndex[0]-1][1];
+                glBegin(GL_TRIANGLES);
+                    glNormal3f (NormalVertices[normalIndex[0]-1][0],NormalVertices[normalIndex[0]-1][1],NormalVertices[normalIndex[0]-1][2]);
+                    glTexCoord2f(TextureVertices[uvIndex[0]-1][0],TextureVertices[uvIndex[0]-1][1]);
+                    glVertex3f (Vertices[vertexIndex[0]-1][0], Vertices[vertexIndex[0]-1][1], Vertices[vertexIndex[0]-1][2]);
+                    glNormal3f (NormalVertices[normalIndex[1]-1][0],NormalVertices[normalIndex[1]-1][1],NormalVertices[normalIndex[1]-1][2]);
+                    glTexCoord2f(TextureVertices[uvIndex[1]-1][0],TextureVertices[uvIndex[1]-1][1]);
+                    glVertex3f (Vertices[vertexIndex[1]-1][0], Vertices[vertexIndex[1]-1][1], Vertices[vertexIndex[1]-1][2]);
+                    glNormal3f (NormalVertices[normalIndex[2]-1][0],NormalVertices[normalIndex[2]-1][1],NormalVertices[normalIndex[2]-1][2]);
+                    glTexCoord2f(TextureVertices[uvIndex[2]-1][0],TextureVertices[uvIndex[2]-1][1]);
+                    glVertex3f (Vertices[vertexIndex[2]-1][0], Vertices[vertexIndex[2]-1][1], Vertices[vertexIndex[2]-1][2]);
+                glEnd();
+                glDisable(GL_TEXTURE_2D); //habilitar textura
+            }
+            else {
+                glColor4fv(colors[cor]);
+                glBegin(GL_TRIANGLES);
+                    glNormal3f (NormalVertices[normalIndex[0]-1][0],NormalVertices[normalIndex[0]-1][1],NormalVertices[normalIndex[0]-1][2]);
+                    glVertex3f (Vertices[vertexIndex[0]-1][0], Vertices[vertexIndex[0]-1][1], Vertices[vertexIndex[0]-1][2]);
+                    glNormal3f (NormalVertices[normalIndex[1]-1][0],NormalVertices[normalIndex[1]-1][1],NormalVertices[normalIndex[1]-1][2]);
+                    glVertex3f (Vertices[vertexIndex[1]-1][0], Vertices[vertexIndex[1]-1][1], Vertices[vertexIndex[1]-1][2]);
+                    glNormal3f (NormalVertices[normalIndex[2]-1][0],NormalVertices[normalIndex[2]-1][1],NormalVertices[normalIndex[2]-1][2]);
+                    glVertex3f (Vertices[vertexIndex[2]-1][0], Vertices[vertexIndex[2]-1][1], Vertices[vertexIndex[2]-1][2]);
+                glEnd();
+            }
         }
     }
-/*	PARSER ANTIGO
-    GLdouble V;
-	char carac;
-	int counter = 0;
-	double i = 0;
-    string lixo;
-    for(int i=0;i<4;i++)
-        getline(f,lixo);
-	while(true){
-		f>>carac;
-		if(carac != 'v')
-			break;
-		vector<GLdouble> vertice;
-		f>>V;
-		vertice.push_back(V);
-		f>>V;
-		vertice.push_back(V);
-		f>>V;
-		vertice.push_back(V);
-		Vertices.push_back(vertice);
-		counter++;
-	}
-	int num1,num2,num3;
-	int conta = 0;
-    int lixo2;
-    if (carac == 's') { //remover o smoth
-        f>>lixo2;
-        f>>carac;
-    }
-	while(!f.eof()){
-		f>>num1;f>>num2;f>>num3;
-        if (cor != 0)
-            glColor4fv(colors[cor]);
-        else
-		    glColor4f(0,0,0,0.9);
-		glBegin(GL_TRIANGLES);
-			glVertex3f (Vertices[num1-1][0], Vertices[num1-1][1], Vertices[num1-1][2]);
-			glVertex3f (Vertices[num2-1][0], Vertices[num2-1][1], Vertices[num2-1][2]);
-			glVertex3f (Vertices[num3-1][0], Vertices[num3-1][1], Vertices[num3-1][2]);
-		glEnd();
-		f>>carac;
-        if(carac == 's') f>>lixo2; //remover smoth entre faces
-		conta++;
-    }
-    */
 	fclose (f);
+}
 
+void makeCheckImage(void){
+    int i, j, c;
+    for (i = 0; i < checkImageWidth; i++) {
+        for (j = 0; j < checkImageHeight; j++) {
+            c = ((((i&0x8)==0)^((j&0x8)==0)))*255;            
+            checkImage[i][j][0] = (GLubyte) c;
+            checkImage[i][j][1] = (GLubyte) c;
+            checkImage[i][j][2] = (GLubyte) c;
+        }
+    }
+}
+
+int ImageLoad(char *filename, Image *image) {
+    FILE *file;
+    unsigned size; // size of the image in bytes.
+    unsigned long i; // standard counter.
+    unsigned short int planes; // number of planes in image (must be 1)
+    unsigned short int bpp; // number of bits per pixel (must be 24)
+    char temp; // temporary color storage for bgr-rgb conversion.
+    // make sure the file is there.
+    if ((file = fopen(filename, "rb"))==NULL){
+        printf("File Not Found : %s\n",filename);
+        return 0;
+    }
+    // seek through the bmp header, up to the width/height:
+    fseek(file, 18, SEEK_CUR);
+    // read the width
+    if ((i = fread(&image->sizeX, 4, 1, file)) != 1) {
+        printf("Error reading width from %s.\n", filename);
+        return 0;
+    }
+    //printf("Width of %s: %lu\n", filename, image->sizeX);
+    // read the height
+    if ((i = fread(&image->sizeY, 4, 1, file)) != 1) {
+        printf("Error reading height from %s.\n", filename);
+        return 0;
+    }
+    //printf("Height of %s: %lu\n", filename, image->sizeY);
+    // calculate the size (assuming 24 bits or 3 bytes per pixel).
+    size = image->sizeX * image->sizeY * 3;
+    // read the planes
+    if ((fread(&planes, 2, 1, file)) != 1) {
+        printf("Error reading planes from %s.\n", filename);
+        return 0;
+    }
+    if (planes != 1) {
+        printf("Planes from %s is not 1: %u\n", filename, planes);
+        return 0;
+    }
+    // read the bitsperpixel
+    if ((i = fread(&bpp, 2, 1, file)) != 1) {
+        printf("Error reading bpp from %s.\n", filename);
+        return 0;
+    }
+    if (bpp != 24) {
+        printf("Bpp from %s is not 24: %u\n", filename, bpp);
+        return 0;
+    }
+    // seek past the rest of the bitmap header.
+    fseek(file, 24, SEEK_CUR);
+
+    // read the data.
+    image->data = (char *) malloc(size);
+    if (image->data == NULL) {
+        printf("Error allocating memory for color-corrected image data");
+        return 0;
+    }
+    if ((i = fread(image->data, size, 1, file)) != 1) {
+        printf("Error reading image data from %s.\n", filename);
+        return 0;
+    }
+    // for (i=0;i<size;i+=3) { // reverse all of the colors. (bgr -> rgb)
+    //     temp = image->data[i];
+    //     image->data[i] = image->data[i+2];
+    //     image->data[i+2] = temp;
+    // }
+    // we're done.
+    return 1;
+}
+Image * loadTexture(char *file_name){
+    Image *image_aux;
+    // allocate space for texture
+    image_aux = (Image *) malloc(sizeof(Image));
+    if (image_aux == NULL) {
+        printf("Error allocating space for image");
+        exit(0);
+    }
+    if (!ImageLoad(file_name, image_aux)) {
+        exit(1);
+    }
+    return image_aux;
 }
 
 void texto(string s,float x,float y,float z) //func de texto usando bitmap
@@ -254,7 +336,7 @@ void init(void) {
     GLfloat mat_specular[ ] = { 0.0, 1.0, 1.0, 1.0 };
     GLfloat mat_shininess[ ] = { 1.0, 1.0, 1.0, 1.0 };
     GLfloat light_position[ ] = { 0.0, 25.0, 0.0, 0.0 };
-    GLfloat white_light[ ] = { 1.0, 1.0, 1.0, 0.0 };
+    GLfloat white_light[ ] = { 0.3, 0.3, 0.3, 1.0 };
     GLfloat red_light[ ] = { 1.0, 0.0, 0.0, 0.0 };
     glClearColor (1.0, 1.0, 1.0, 1.0);
     // glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
@@ -263,8 +345,74 @@ void init(void) {
     // glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
     glLightfv(GL_LIGHT1, GL_POSITION, light_position);
     glLightfv(GL_LIGHT1, GL_AMBIENT, white_light);
-    glLightfv(GL_LIGHT1, GL_DIFFUSE, white_light);
+    // glLightfv(GL_LIGHT1, GL_DIFFUSE, white_light);
     //glLightfv(GL_LIGHT0, GL_SPECULAR, white_light);
+
+    GLfloat light2_position[ ] = {viewer[0], viewer[1], viewer[2], 1.0};
+    GLfloat light2_direction[ ] = {viewVector[0], viewVector[1], viewVector[2], 1.0};
+    glLightfv(GL_LIGHT2, GL_POSITION, light2_position);
+    cerr << light2_position[0] << " " << light2_direction[0] << endl;
+    glLightfv(GL_LIGHT2, GL_SPOT_DIRECTION, light2_direction);
+    glLighti(GL_LIGHT2, GL_SPOT_EXPONENT, 2);
+    // glLightf(GL_LIGHT2, GL_CONSTANT_ATTENUATION, 4.0);
+    // glLightf(GL_LIGHT2, GL_LINEAR_ATTENUATION, 0.001);
+    glLighti(GL_LIGHT2, GL_SPOT_CUTOFF, 30);
+    glLightfv(GL_LIGHT2, GL_DIFFUSE, red_light);
+
+    //8 Texturas
+    Image *image1 = loadTexture("Textures/Metal.bmp");
+    Image *image2 = loadTexture("Textures/Madeira.bmp"); 
+    Image *image3 = loadTexture("Textures/Areia.bmp"); 
+    Image *image4 = loadTexture("Textures/Escamas.bmp"); 
+    Image *image5 = loadTexture("Textures/Abacaxi.bmp"); 
+    if(image1 == NULL || image2 == NULL || image3 == NULL || image4 == NULL){
+        printf("Image was not returned from loadTexture\n");
+        exit(0);
+    }
+    makeCheckImage();
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+    // Create Texture
+    //Primeira Textura - Metal
+    glGenTextures(8, texture);
+    glBindTexture(GL_TEXTURE_2D, texture[0]);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR); //scale linearly when image bigger than texture
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR); //scale linearly when image smalled than texture
+    glTexImage2D(GL_TEXTURE_2D, 0, 3, image1->sizeX, image1->sizeY, 0,
+                 GL_RGB, GL_UNSIGNED_BYTE, image1->data);
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
+ 
+    //Segunda textura - Madeira
+    glBindTexture(GL_TEXTURE_2D, texture[1]);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR); //scale linearly when image bigger than texture
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR); //scale linearly when image smalled than texture
+    glTexImage2D(GL_TEXTURE_2D, 0, 3, image2->sizeX, image2->sizeY, 0,
+                 GL_RGB, GL_UNSIGNED_BYTE, image2->data);
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_BLEND);
+    
+    //Terceira textura - Areia
+    glBindTexture(GL_TEXTURE_2D, texture[2]);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR); //scale linearly when image bigger than texture
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR); //scale linearly when image smalled than texture
+    glTexImage2D(GL_TEXTURE_2D, 0, 3, image3->sizeX, image3->sizeY, 0,
+                 GL_RGB, GL_UNSIGNED_BYTE, image3->data);
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
+
+    //Quarta textura - Escamas
+    glBindTexture(GL_TEXTURE_2D, texture[3]);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR); //scale linearly when image bigger than texture
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR); //scale linearly when image smalled than texture
+    glTexImage2D(GL_TEXTURE_2D, 0, 3, image4->sizeX, image4->sizeY, 0,
+                 GL_RGB, GL_UNSIGNED_BYTE, image4->data);
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
+
+    //Quinta textura - Abacaxi
+    glBindTexture(GL_TEXTURE_2D, texture[4]);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR); //scale linearly when image bigger than texture
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR); //scale linearly when image smalled than texture
+    glTexImage2D(GL_TEXTURE_2D, 0, 3, image5->sizeX, image5->sizeY, 0,
+                 GL_RGB, GL_UNSIGNED_BYTE, image5->data);
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
 }
 
 void Temporizador (int tempo) { //funcao para dar mais fluides aos movimentos, incrementa todas
@@ -428,36 +576,49 @@ void display(void) {
 		0.0, 1.0, 0.0);
     glPushMatrix();
 
+    GLfloat red_light[ ] = { 1.0, 1.0, 1.0, 1.0 };
+    GLfloat light2_position[ ] = {viewer[0], viewer[1], viewer[2], 1.0};
+    GLfloat light2_direction[ ] = {viewVector[0], viewVector[1], viewVector[2], 1.0};
+    glLightfv(GL_LIGHT2, GL_POSITION, light2_position);
+    // cerr << light2_position[0] << " " << light2_direction[0] << endl;
+    glLightfv(GL_LIGHT2, GL_SPOT_DIRECTION, light2_direction);
+    glLighti(GL_LIGHT2, GL_SPOT_EXPONENT, 2);
+    // glLightf(GL_LIGHT2, GL_CONSTANT_ATTENUATION, 4.0);
+    // glLightf(GL_LIGHT2, GL_LINEAR_ATTENUATION, 0.001);
+    glLighti(GL_LIGHT2, GL_SPOT_CUTOFF, 30);
+    
+    glLightfv(GL_LIGHT2, GL_DIFFUSE, red_light);
+
     glTranslated(center[0], center[1], center[2]);
 	glRotatef(-1*Rot,0,1,0);  //fazer translacao pra rodar direito
     glRotatef(90,0,1,0);
     glTranslated(-1*center[0], -1*center[1], -1*center[2]);
     glTranslated(center[0], center[1], center[2]);
-
-    Parser("models/submarino2.obj", 0); //0 = sem cor
-    glPopMatrix();	
-
-    glPushMatrix();
     
+    Parser("models/submarino3.obj", 0,0); //0 = sem cor 0 = metal
+    glPopMatrix();	
+    
+    glPushMatrix();
         glTranslatef(0,-63,0);
         glScalef(6,1,6);
-    
-    Parser("models/areia2.obj",7); //7 = amarelo
+    Parser("models/areia2.obj",7,2); //7 = amarelo; 2 = areia
     glPopMatrix();
-    
+
     glPushMatrix();{
         glTranslatef(-6,-73.7,-3);
         glScalef(1.5,1.5,1.5);
     }
-    Parser("models/tesouro2.obj",6); //marrom 
+    Parser("models/tesouro2.obj",6,1); //6 = marrom; 1 = madeira
     glPopMatrix(); 
+    
     glPushMatrix();{
         glTranslatef(12,0,0);
         glScalef(2,2,2);
         glTranslatef(0,animaNavio,0);
     }
-    Parser("models/navio2.obj",6);
+    Parser("models/navio2.obj",6,1);  //6 = marrom; 1 = madeira
     glPopMatrix(); 
+    
     glPushMatrix();{
         glTranslatef(-45,-50,6);
         glScalef(2,2,2);
@@ -468,13 +629,14 @@ void display(void) {
             glTranslatef(-15,-10,6);
         }
     }
-    Parser("models/cardumao2.obj",0); 
+    Parser("models/cardumao2.obj",0,3); //3 = escamas
     glPopMatrix();
+    
     glPushMatrix();{
         glTranslatef(28,-75,-3);
         glScalef(6,8,6);
     }
-    Parser("models/abacaxi2.obj",3); //laranja
+    Parser("models/abacaxi2.obj",3,4); //laranja - 4 = Abacaxi
     glPopMatrix();
     
     glPushMatrix();{
@@ -483,21 +645,21 @@ void display(void) {
 		glScalef(0.2,0.2,0.2);
         glTranslatef(0,animaNavio,0);
     }
-    Parser("models/jetski2.obj",5); //cinza
+    Parser("models/jetski2.obj",5,0); //cinza
     glPopMatrix();
-
-    // Funcao extra, nao finalizada
-  /*  if (torpedo) { 
-        glPushMatrix();{
-            double posTorpedo[3] = {center[0],center[1],center[2]};
-            //glTranslatef(0,PosY_torp,PosX_torp);
-            // for(int i=0;)
-            glTranslatef(animaTorpedo*viewVector[0], 0, animaTorpedo*viewVector[2]);
-        }
-        Parser("models/torpedo.obj",0); //cinza
-        glPopMatrix();
-    }
-	*/
+    
+//     // Funcao extra, nao finalizada
+//   /*  if (torpedo) { 
+//         glPushMatrix();{
+//             double posTorpedo[3] = {center[0],center[1],center[2]};
+//             //glTranslatef(0,PosY_torp,PosX_torp);
+//             // for(int i=0;)
+//             glTranslatef(animaTorpedo*viewVector[0], 0, animaTorpedo*viewVector[2]);
+//         }
+//         Parser("models/torpedo.obj",0); //cinza
+//         glPopMatrix();
+//     }
+// 	*/
     
     colorcube(); //desenha o cubo
     if(showmenu && POV==1)         //caso 'h' seja pressionado chama o menu do ponto de vista atua
@@ -582,7 +744,6 @@ int main(int argc, char **argv) {
     glEnable(GL_FLAT | GL_SMOOTH);
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_COLOR_MATERIAL);
-    glEnable(GL_TEXTURE_2D); //habilitar textura
     glEnable(GL_BLEND); //habilita o alpha nas superficies
     glutTimerFunc(100, Temporizador,0); //determina funcao de Timer
     glutMainLoop(); //loop
